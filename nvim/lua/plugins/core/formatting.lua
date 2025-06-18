0236e8969b8c5e811ea622dccc9dc17b020ef1bb
Alex Llanez <xljdev@proton.me>
2025-06-17 22:12:18 -0600

    chore: add php formatter to configuration

--- nvim/lua/plugins/core/formatting.lua
+++ nvim/lua/plugins/core/formatting.lua
@@ -21,6 +21,7 @@ return {
 				graphql = { "prettier" },
 				liquid = { "prettier" },
 				lua = { "stylua" },
+				php = { "php-cs-fixer" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
@@ -34,7 +35,6 @@ return {
 				lsp_fallback = true,
 				async = false,
 				timeout_ms = 1000,
-
 			})
 		end, { desc = "Format file or range (in visual mode)" })
 	end,
