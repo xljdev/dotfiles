6117b5381afba146ebcb6eec0e849d97cc0f8e17
Alex Llanez <xljdev@proton.me>
2025-06-25 10:40:12 -0600

    feat: Add rounded border to Mason main buffer

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,52 +1,51 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-        -- import mason
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
 
-        local mason = require("mason")
+		local mason = require("mason")
 
-        -- import mason-lspconfig
+		-- import mason-lspconfig
 
-        local mason_lspconfig = require("mason-lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
 
-        local mason_tool_installer = require("mason-tool-installer")
+		local mason_tool_installer = require("mason-tool-installer")
 
-        -- enable mason and configure icons
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-        })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+				border = "rounded",
+			},
+		})
 
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"html",
+				"cssls",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"vuels",
+			},
+		})
 
-        mason_lspconfig.setup({
-            -- list of servers for mason to install
-            ensure_installed = {
-                "html",
-                "cssls",
-                "lua_ls",
-                "graphql",
-                "emmet_ls",
-                "vuels",
+		mason_tool_installer.setup({
+			ensure_installed = {
 
-            },
-        })
-
-        mason_tool_installer.setup({
-            ensure_installed = {
-
-                "prettier", -- prettier formatter
-                "stylua",   -- lua formatter
-                "eslint_d",
-            },
-        })
-    end,
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"eslint_d",
+			},
+		})
+	end,
 }
