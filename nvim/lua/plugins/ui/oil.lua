46574a46e74559b162db8b51d1b8541dbfa866fc
Alex Llanez <xljdev@proton.me>
2025-06-25 12:17:40 -0600

    fix: Rename tmux configuration file

--- nvim/lua/plugins/ui/oil.lua
+++ nvim/lua/plugins/ui/oil.lua
@@ -1,18 +1,28 @@
 return {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
 
-    config = function()
-        require("oil").setup {
-            columns = { "icon" }
-        }
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    end
+	config = function()
+		require("oil").setup({
+			columns = { "icon" },
+			default_file_explorer = true,
+			view_options = {
+				show_hidden = true,
+				is_hidden_file = function(name, bufnr)
+					return vim.startswith(name, ".")
+				end,
+				is_always_hidden = function(name, bufnr)
+					return false
+				end,
+			},
+		})
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
 }
